6f90f9f332367489a41b736f3ac7018dd64b649e
KervinJean55 <142912642+KervinJean55@users.noreply.github.com>
2023-09-29 09:24:37 -0500

    webServer assignment

--- webServer.py
+++ webServer.py
@@ -1,3 +1,4 @@
+
 # import socket module
 from socket import *
 # In order to terminate the program
@@ -23,39 +24,31 @@ def webServer(port=13331):
     
     try:
       message = connectionSocket.recv(1024).decode()#Fill in start -a client is sending you a message   #Fill in end 
-      filename = message.split()[1][1:]
+      filename = message.split()[1]
       
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
       f = open(filename[1:], "rb") #fill in start #fill in end)
-
       #fill in end
       
 
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
       #Fill in start 
-      with open(filename, "rb") as f:
-        outputdata = f.read()      
+              
       #Content-Type is an example on how to send a header as bytes. There are more!
-     
-      
+      outputdata = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-      response_headers = "HTTP/1.1 200 OK\r\n"
-      response_headers += "Content-Type: text/html; charset=UTF-8\r\n"
-      response_headers += "\r\n"
-      
-      
-      connectionSocket.send(response_headers.encode())
       connectionSocket.send(outputdata)
       #Fill in end
                
-       #for line in file
+      for i in f: #for line in file
       #Fill in start - append your html file contents #Fill in end 
-       
+        connectionSocket.send(i)
       #Send the content of the requested file to the client (don't forget the headers you created)!
       # Fill in start
-      
+      f.close()
       
 
       # Fill in end
