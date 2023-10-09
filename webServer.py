ba25e88c64abfadee7901278a88ae5e20c1a85e5
KervinJean55 <142912642+KervinJean55@users.noreply.github.com>
2023-10-09 17:01:37 -0500

    smtp assignment

--- webServer.py
+++ webServer.py
@@ -8,7 +8,7 @@ import sys
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   
-  #Prepare a server socket
+  #Preparing a server socket
   serverSocket.bind(("", port))
   
   #Fill in start
@@ -16,7 +16,7 @@ def webServer(port=13331):
   #Fill in end
 
   while True:
-    #Establish the connection
+    #Establishing the connection
     
     print('Ready to serve...')
     connectionSocket, addr =  serverSocket.accept()#Fill in start -are you accepting connections?     #Fill in end
@@ -26,20 +26,20 @@ def webServer(port=13331):
             filename = message.split()[1]
 
             try:
-                # Open the client requested file
+                # Opening the client requested file
                 f = open(filename[1:], "rb")
                 outputdata = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
 
-                # Read the file content and add it to the response
+                # Reading the file content and add it to the response
                 file_content = f.read()
                 response = outputdata + file_content
 
-                # Send the entire response to the client
+                # Sending the entire response to the client
                 connectionSocket.send(response)
                 f.close()
                 connectionSocket.close()
             except FileNotFoundError:
-                # Send a 404 response if the file is not found
+                # Sends a 404 response if the file is not found
                 not_found_response = "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found"
                 connectionSocket.send(not_found_response.encode())
                 connectionSocket.close()
